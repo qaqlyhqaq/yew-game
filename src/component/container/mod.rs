97efd6f8ec69b9bd2c2032e28b0196f070d33926
lyh <liaoyinghai@yymt.com>
2025-03-20 16:39:21 +0800

    submit !

--- src/component/container/mod.rs
+++ src/component/container/mod.rs
@@ -15,14 +15,6 @@ use yew::{function_component, html, use_context, use_reducer, Callback, Componen
 use crate::component::container::collapsible::Collapsible;
 use crate::component::container::trConponent::{TrConponent, TrProperties};
 
-/// 表达一个导出module 容器,根据获取父组件id,来记录组件
-///  大小,夫组件id,以及其他详细内容
-pub struct Container {
-    //构造大小字段
-    size: (u32, u32),
-    //父容器id
-    parent_id: String,
-}
 
 pub enum ActiveWrapper {
     //按钮点击操作
@@ -31,15 +23,6 @@ pub enum ActiveWrapper {
     TotalCountStateUpdate(HashMap<String, usize>),
 }
 
-/// 实现一个从 父容器id 字符串构造 容器方法
-impl From<String> for Container {
-    fn from(item: String) -> Container {
-        Container {
-            size: (0, 0),
-            parent_id: item,
-        }
-    }
-}
 
 // 定义共享状态结构体
 #[derive(Clone, PartialEq, Debug, Eq, Derivative)]
